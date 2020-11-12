97aab29543c912853084bf08a734558f825fe1b7
gvnikhil21 <69467502+gvnikhil21@users.noreply.github.com>
2020-11-13 01:50:18 +0530

    Refactored: Code refactored to validate contact attributes in the setter methods to reduce lines of code

--- addressBook.js
+++ addressBook.js
@@ -1,5 +1,5 @@
 import { Contact } from "./contact.js";
-import * as validation from "./validation.js";
+
 import { createRequire } from "module";
 const require = createRequire(import.meta.url);
 const prompt = require('prompt-sync')({ sigint: true });
@@ -128,28 +128,20 @@ let countByState = () => {
 let editOrAddContact = (contact) => {
     try {
         let firstName = prompt("Enter first name: ");
-        validation.validateName(firstName);
         contact.firstName = firstName;
         let lastName = prompt("Enter last name: ");
-        validation.validateName(lastName);
         contact.lastName = lastName;
         let address = prompt("Enter address: ");
-        validation.validateAddress(address);
         contact.address = address;
         let city = prompt("Enter city: ");
-        validation.validateCity(city);
         contact.city = city;
         let state = prompt("Enter state: ");
-        validation.validateState(state);
         contact.state = state;
         let zip = prompt("Enter zip: ");
-        validation.validateZip(zip);
         contact.zip = zip;
         let phone = prompt("Enter phone no.: ");
-        validation.validatePhone(phone);
         contact.phone = phone;
         let email = prompt("Enter email-id: ");
-        validation.validateEmail(email);
         contact.email = email;
         return true;
     } catch (error) {
