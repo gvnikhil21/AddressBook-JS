730b903bead48bd67e7b987cb978aad283f838c2
gvnikhil21 <69467502+gvnikhil21@users.noreply.github.com>
2020-11-12 22:33:41 +0530

    Added: Get contacts by city or state functionality added

--- AddressBookMain.js
+++ AddressBookMain.js
@@ -5,7 +5,7 @@ const prompt = require('prompt-sync')({ sigint: true });
 
 let choice = 0;
 do {
-    console.log("\n1. Add Contact\n2. Display contacts\n3. Find Contact By Name\n4. Edit Contact By Name\n5. Delete Contact by Name\n6. Count contacts in AddressBook\n7. Exit");
+    console.log("\n1. Add Contact\n2. Display contacts\n3. Find Contact By Name\n4. Edit Contact By Name\n5. Delete Contact by Name\n6. Count contacts in AddressBook\n7. Get Contacts by City\n8. Get Contacts by State\n9. Exit");
     choice = Number(prompt("Enter your choice number: "));
     switch (choice) {
         case 1:
@@ -27,10 +27,16 @@ do {
             addressBook.countContacts();
             break;
         case 7:
+            addressBook.getContactsByCity();
+            break;
+        case 8:
+            addressBook.getContactsByState();
+            break;
+        case 9:
             console.log("Thank you! You have now quit the program.");
             break;
         default:
             console.log("Invalid Choice! Enter proper choice number");
             break;
     }
-} while (choice != 7);
+} while (choice != 9);
